960a5e1472fa2dccd48ba86c477b112f3b749c18
SamuelColmaan <samuelcolman07@gmail.com>
2024-11-05 17:01:12 -0300

    Iniciando gulp

---
+++ 11.gulp/gulpfile.js
@@ -0,0 +1,20 @@
+function funcaoPadrao(callback){
+    console.log('Teste de gulp');
+    callback();
+}
+
+function dizOi(callback) {
+    console.log('Hello, world')
+    dizTchau()
+    callback()
+}
+
+function dizTchau(){
+    console.log('Tchau gulp')
+}
+
+exports.default = funcaoPadrao;
+exports.dizOi = dizOi;
+
+/* Na hora de chamar a tarefa default não precisa especificar. Pode colocar no terminal: npm run gulp */
+/* Mas a tarefa que não é default precisa ser chamada com o nome dela no terminal. Por exemplo: npm run gulp dizOi */
