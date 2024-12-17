41be88c96e6ae65f9917f263557737e08327de45
SamuelColmaan <samuelcolman07@gmail.com>
2024-12-17 16:32:10 -0300

    Atualização 2.0 do código

--- 1.python/testes/number.py
+++ 1.python/testes/number.py
@@ -1,14 +1,14 @@
 import time
 
-# Iniciar o contador e armazenar o tempo de início
-contador = 0
-inicio = time.time()  # Tempo inicial
 
-# Loop para contar até que um segundo se passe
+contador = 0
+inicio = time.time()  
+
+
 while True:
-    contador += 1  # Incrementa o contador
-    if time.time() - inicio >= 1:  # Verifica se já se passaram 1 segundo
-        break  # Sai do loop
+    contador += 1
+    if time.time() - inicio >= 1:
+        break
 
 
 print(f"O computador contou até {contador} em 1 segundo.")
--- 11.gulp/gulpfile.js
+++ 11.gulp/gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const sourcemaps = require('gulp-sourcemaps')
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+
+function comprimeJavaScript() {
+    return gulp.src ('./source/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/scripts'))
+} 
 
 function compilaSass() {
     return gulp.src('./source/styles/main.scss')
@@ -36,6 +43,7 @@ exports.watch = function() {
     gulp.watch('./source/styles/*.scss', {ignoreInitial: false}, gulp.series(compilaSass));
 }
 
+exports.javascript = comprimeJavaScript
 //exports.default = gulp.parallel(funcaoPadrao, sayHi) // executando em paralelo
 
 
