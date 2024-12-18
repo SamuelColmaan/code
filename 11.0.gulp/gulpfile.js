7b20999e53ad597e1df359d17d4fa8ecc37faecc
SamuelColmaan <samuelcolman07@gmail.com>
2024-12-18 16:54:15 -0300

    Separação do trabalho

--- 11.0.gulp/gulpfile.js
+++ 11.0.gulp/gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+const sass = require('gulp-sass')(require('sass')); 
 const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
 const obfuscate = require ('gulp-obfuscate'); //tornar um arquivo ilegível
