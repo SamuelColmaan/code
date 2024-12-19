94e72e6cdbf84531003374a6cb89598817649d17
SamuelColmaan <samuelcolman07@gmail.com>
2024-12-19 11:50:33 -0300

    Navegador verificar o arquivo intermediário ao inves do final no localização do css

--- 11.1.exercicio/gulpfile.js
+++ 11.1.exercicio/gulpfile.js
@@ -1,14 +1,17 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass')); //compilação composta. O primeiro pacote é resposável por integrar o sass com o gulp (gulp-sass), mas quem vai fazer toda a compilação é o segundo pacote (sass). 
+const sourcemaps = require('gulp-sourcemaps'); 
 
 // A execução da função compilaSass:
 // O .src é para pegar o aquivo fonte;
 // O pipe é para de fato compilar
 function compilaSass(){
     return gulp.src('./source/styles/main.scss') // Colocar o caminho das pastas.
+        .pipe(sourcemaps.init()) 
         .pipe(sass({
             outputStyle: 'compressed'
         }))//aqui dentro é chamando a variável que fez a requisição dos pacotes.
+        .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest('./build/styles/')); // Colocar o caminho da pasta de destino da compilação
 }
 
