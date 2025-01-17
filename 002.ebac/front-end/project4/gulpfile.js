e86b570a56e134cc189d498bf87d8934ac805d1d
SamuelColmaan <samuelcolman07@gmail.com>
2025-01-17 15:55:52 -0300

    Continuação do projeto

--- 002.ebac/front-end/project4/gulpfile.js
+++ 002.ebac/front-end/project4/gulpfile.js
@@ -1,11 +1,16 @@
 const gulp = require('gulp'); // Import gulp
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin')
 
 function styles() {
     return gulp.src('./src/styles/*.scss').pipe(sass({outputStyle: 'compressed'})).pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+function images() {
+    return gulp.src('./src/images/**/*').pipe(imagemin()).pipe(gulp.dest('./dist/images')); /* You Have to use gulp-imagemin@7.1.0 */
+}
+
+exports.default = gulp.parallel(styles, images);
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
